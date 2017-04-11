3c6550fdec677b84f805c5fd1fbca0feee71a999
Michael Jones <mjones5811@aol.com>
2017-04-11 17:12:23 -0400

    updates

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,4 +1,26 @@
+var config = {
+    apiKey: "AIzaSyB0B7n5RShoXKGzqddL0JNxunQfvHBbcRs",
+    authDomain: "project-5759926021839573065.firebaseapp.com",
+    databaseURL: "https://project-5759926021839573065.firebaseio.com",
+    projectId: "project-5759926021839573065",
+    storageBucket: "project-5759926021839573065.appspot.com",
+    messagingSenderId: "173548131166"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+var usr = "";
+
 $( document ).ready(function(){
+
+database.ref().on("value", function(snapshot) {
+  var sv = snapshot.val();
+  console.log(sv.usrName);
+  $("#profileName").append("<div>" + snapshot.val().usrName+ "<div>");
+},
+function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    });
 
 var myArray = [];
 		function ingredientsAdd(){
@@ -24,95 +46,97 @@ var myArray = [];
 	ingredientsAdd();
 
 
-function signUpEmail(){
+
 
   // Initialize Firebase
- var config = {
-    apiKey: "AIzaSyB0B7n5RShoXKGzqddL0JNxunQfvHBbcRs",
-    authDomain: "project-5759926021839573065.firebaseapp.com",
-    databaseURL: "https://project-5759926021839573065.firebaseio.com",
-    projectId: "project-5759926021839573065",
-    storageBucket: "project-5759926021839573065.appspot.com",
-    messagingSenderId: "173548131166"
-  };
-  firebase.initializeApp(config);
-  var database = firebase.database();
-  var auth = firebase.auth();
+ 
   
+  var auth = firebase.auth();
+
+function signUpEmail(){ 
+
   function clearlogIn(){
     $("#txtEmail").val("");
     $("#txtPassWord").val("");
   }
   
-function setLocalStorage(){
-   $("#logInEmail").html();
-      $("#logInEmail").html("Signed is As: " + email);
-      localStorage.clear();
-      localStorage.setItem("email",email);
-     
-}
- $("#logInEmail").html("Signed is As: " + localStorage.getItem("email"));
-// to log in as a existng usr 
-$("#btnLogin").on("click", e=> {
-     event.preventDefault();
-     email = $("#txtEmail").val().trim();
-     pass = $("#txtPassword").val().trim();
-      console.log(email);
-     setLocalStorage();
-     
+    function setLocalStorage(){
+       $("#logInEmail").html();
+          $("#logInEmail").html("Signed is As: " + email);
+          localStorage.clear();
+          localStorage.setItem("email",email);
+         
+    }
+    
+    // to log in as a existng usr 
+    $("#btnLogin").on("click", e=> {
+         event.preventDefault();
+         email = $("#txtEmail").val().trim();
+         pass = $("#txtPassword").val().trim();
+          console.log(email);
+         setLocalStorage();
+         
 
-    var auth = firebase.auth();
-    clearlogIn();
-    var promise = auth.signInWithEmailAndPassword(email,pass);
+        var auth = firebase.auth();
+        clearlogIn();
+        var promise = auth.signInWithEmailAndPassword(email,pass);
 
-    promise.catch(e => console.log(e.message));
+        promise.catch(e => console.log(e.message));
 
- });
- 
-  // to sign up for an account
-  $("#btnSignUp").on("click", e=>{
-    // TODO CHECK FOR REAL EMAIL
-    event.preventDefault();
-     email = $("#email").val().trim();
-     pass = $("#pwd").val().trim();
-     usr = $("#usr").val().trim();
-     auth = firebase.auth();
-    console.log(email);
-    console.log(pass);
-    console.log(usr);
-
-    var promise = auth.createUserWithEmailAndPassword(email,pass);
-    promise.catch(e => console.log(e.message));
-    promise.catch(e => $("#dupMessage").html(e.message + "Click "+"<a href = 'indexSignUpPage.html'>here</a> "+"to try again"));
-    $("#dupMessage").html("Click "+"<a href = 'index.html'>here</a> "+"here to log in");
-    setLocalStorage();
      });
-  // to log out of user account
-  $("#btnLogOut").on("click",e =>{
-      firebase.auth().signOut();
-      localStorage.clear();
-      $("#logInEmail").html("");
-  });
-  //checks to see if the user is logged in or out
-  firebase.auth().onAuthStateChanged(firebaseUser =>{
-      if(firebaseUser){
-       console.log(firebaseUser);
-       $("#txtEmail").addClass("hide");
-        $("#userEmail").removeClass("hide");
-        $("#txtPassword").addClass("hide");
-        $("#btnLogOut").removeClass("hide");
-        $("#logInEmail").removeClass("hide");
-      }
-      else  {
-        console.log("not logged in");
-        $("#txtEmail").removeClass("hide");
-        $("#userEmail").addClass("hide");
-        $("#txtPassword").removeClass("hide");
-        $("#btnLogOut").addClass("hide");
-        $("#logInEmail").addClass("hide");
-      }
-  });
-}
+     
+      // to sign up for an account
+      $("#btnSignUp").on("click", e=>{
+        // TODO CHECK FOR REAL EMAIL
+        event.preventDefault();
+         email = $("#email").val().trim();
+         pass = $("#pwd").val().trim();
+         usrName = $("#usr").val().trim();
+         auth = firebase.auth();
+        console.log(email);
+        console.log(pass);
+        console.log(usr);
+        database.ref().push({
+          usrName: usrName,
+          email: email,
+        });
+        var promise = auth.createUserWithEmailAndPassword(email,pass);
+        promise.catch(e => console.log(e.message));
+        promise.catch(e => $("#dupMessage").html(e.message + "Click "+"<a href = 'indexSignUpPage.html'>here</a> "+"to try again"));
+        $("#dupMessage").html("Click "+"<a href = 'index.html'>here</a> "+"here to log in");
+        setLocalStorage();
+         });
+      // to log out of user account
+      $("#btnLogOut").on("click",e =>{
+          firebase.auth().signOut();
+          localStorage.clear();
+          $("#logInEmail").html("");
+      });
+      //checks to see if the user is logged in or out
+      firebase.auth().onAuthStateChanged(firebaseUser =>{
+          if(firebaseUser){
+           console.log(firebaseUser);
+           $("#txtEmail").addClass("hide");
+            $("#userEmail").removeClass("hide");
+            $("#txtPassword").addClass("hide");
+            $("#btnLogOut").removeClass("hide");
+            $("#logInEmail").removeClass("hide");
+          }
+          else  {
+            console.log("not logged in");
+            $("#txtEmail").removeClass("hide");
+            $("#userEmail").addClass("hide");
+            $("#txtPassword").removeClass("hide");
+            $("#btnLogOut").addClass("hide");
+            $("#logInEmail").addClass("hide");
+          }
+      });
+       $("#logInEmail").html("Signed is As: " + localStorage.getItem("email"));
+  }
 signUpEmail();
-});
+
+
+}); // document on ready
+
+
 
