8fe8e054bd0845d93fe56696f1edbcd3bad0b3bd
john sander <john sander>
2017-04-13 19:30:40 -0400

    fix recipe search

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -34,7 +34,7 @@ var myArray = [];
 				a.text(myArray[i]);
 				a.addClass("meals");
         a.text(" - "+ myArray[i]);
-        
+
 				$("#ingredientList").append(a);
 
 			}
@@ -78,10 +78,10 @@ function removeIngredient(){
 $(document).on("click", ".meals", removeIngredient);
 
 
-  
+
   var auth = firebase.auth();
 
-function signUpEmail(){ 
+function signUpEmail(){
 
 
   function clearlogIn(){
@@ -89,15 +89,15 @@ function signUpEmail(){
     $("#txtPassword").val("");
   }
 
-  
+
     function setLocalStorage(){
        $("#logInEmail").html();
           $("#logInEmail").html("Signed is As: " + email);
           localStorage.clear();
           localStorage.setItem("email",email);
-         
+
     }
-    
+
     // to log in as a existng usr con
     $("#btnLogin").on("click", e=> {
          event.preventDefault();
@@ -108,13 +108,13 @@ function signUpEmail(){
         clearlogIn();
 
         var promise = auth.signInWithEmailAndPassword(email,pass);
-        
+
         promise.catch(e => console.log(e.message));
 
      });
-     
+
       // to sign up for an account
-     
+
 
       // to log out of user account
       $("#btnLogOut").on("click",e =>{
@@ -127,7 +127,7 @@ function signUpEmail(){
       //checks to see if the user is logged in or out
       firebase.auth().onAuthStateChanged(firebaseUser =>{
           if(firebaseUser){
-           
+
           // var displayName = firebaseUser.displayName;
           // var email = firebaseUser.email;
           // var emailVerified = firebaseUser.emailVerified;
@@ -152,7 +152,7 @@ function signUpEmail(){
             $("#logInEmail").addClass("hide");
           }
       });
-      
+
   };
 signUpEmail();
 
@@ -168,7 +168,7 @@ signUpEmail();
             console.log(email);
             console.log(pass);
             console.log(usrName);
-          
+
            firebase.auth().createUserWithEmailAndPassword(email,pass)
            .then(function(user){
             database.ref("/user").push({
@@ -181,17 +181,63 @@ signUpEmail();
             alert(user.uid);
              $("#dupMessage").html("Click "+"<a href = 'index.html'>here</a> "+"here to log in");
             //var promise = auth.createUserWithEmailAndPassword(email,pass);
-          }).catch(function(error){                      
+          }).catch(function(error){
             promise.catch(firebaseUser => console.log(firebaseUser.message));
             promise.catch(firebaseUser => $("#dupMessage").html(firebaseUser.message + "Click "+"<a href = 'indexSignUpPage.html'>here</a> "+"to try again"));
-          
+
             $("#logInEmail").html("Signed is As: " + localStorage.getItem("email"));
         });
         });
 
       }
       signUp();
-  
+
+  $("#ingredientSearch").on("click", function() {
+          event.preventDefault();
+          var userInput = myArray.toString();
+          console.log(userInput);
+          addIngredients(userInput);
+          //return false;
+      });
+
+        //Search function button
+function addIngredients(userInput) {
+    var p = $(this).data("name");
+    var queryURL = "https://api.edamam.com/search?q= " + userInput + " &app_id=694da05d&app_key=56d6789992fbe1f17a7c2ce8cbc3fc62"
+    //ajax call
+    $.ajax({ url: queryURL, method: "GET" })
+        .done(function(response) {
+            var hits = response.hits;
+
+            for (var i = 0; i < hits.length; i++) {
+                var recipe = hits[i].recipe;
+                console.log(recipe);
+                var recipesDiv = $('<div class="item">');
+                var ingredientResults = recipe.ingredientLines;
+                var ingredientParagraph = $("<p>").html("<strong>Ingredients: </strong>" + ingredientResults);
+                var label = recipe.label;
+                var header = $("<h1>").text(label)
+                var image = $("<img>");
+                image.attr("src", recipe.image);
+                var url = $("<a>").html("<p><strong>Click Here for Full Recipe!!!</strong></p>");
+                url.attr("href", recipe.url);
+
+                recipesDiv.append(ingredientParagraph);
+                recipesDiv.append(image);
+                recipesDiv.append(url);
+
+                recipesDiv.prepend(header);
+
+
+
+
+                $("#recipeList").prepend(recipesDiv);
+
+            }
+
+    });
+    // });
+}
 
 }); // do
 
