ff9b91bce451cfbb6ab206db3fc0eefe29e4e4c5
whtmymfnam <mjones5811@aol.com>
2017-04-14 16:19:20 -0400

    Update js.js

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -121,7 +121,7 @@ $(document).on("click", ".meals", removeIngredient);
           $("#profileEmail").append("<div>" + sv[uid].userEmail + "</div>");
           
           $("#profileFavFood").append("<div>" + sv[uid].favFood+ "</div>");
-          $("#profileFavProgrammer").append("<div" + sv[uid].favProgrammer + "</div>");
+          $("#profileFavProgrammer").append("<div>" + sv[uid].favProgrammer + "</div>");
           });
 
            $("#txtEmail").addClass("hide");
